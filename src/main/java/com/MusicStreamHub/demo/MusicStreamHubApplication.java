f2aa253967ffd8dcde0ed794fdce6bc6fd4042c3
PowerUserE <125454293+PowerUserE@users.noreply.github.com>
2023-08-02 22:00:45 +0100

    added springboot materials for testing

--- src/main/java/com/MusicStreamHub/demo/MusicStreamHubApplication.java
+++ src/main/java/com/MusicStreamHub/demo/MusicStreamHubApplication.java
@@ -2,12 +2,27 @@ package com.MusicStreamHub.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.IOException;
+import com.example.spotifyApp.service.ApiRequestService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+//@RestController
 public class MusicStreamHubApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(MusicStreamHubApplication.class, args);
+		System.out.println("Success!");
+
+		ApiRequestService apiRequestService = new ApiRequestService();
+		apiRequestService.makeApiRequest();
 	}
 
 }
+
+
+
+
+
+
